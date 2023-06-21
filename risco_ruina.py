ee2bf543f59c39cf5d4f941f45fb570a91f5d72e
lyssandrosilveira <45230721+lyssandrosilveira@users.noreply.github.com>
2023-06-21 14:24:43 -0300

    risco_ruina.py

--- risco_ruina.py
+++ risco_ruina.py
@@ -8,8 +8,8 @@ def main():
     st.title("Calculadora de Risco de Ruína")
 
     drawdown = st.number_input("Drawdown:", value=-0.87)
-    win_rate = st.number_input("Taxa de Vitória (%):", min_value=0.0, max_value=100.0, value=94.0)
-    bet_size = st.number_input("Tamanho da Aposta:", value=10.0)
+    win_rate = st.number_input("Winrate (%):", min_value=0.0, max_value=100.0, value=94.0)
+    bet_size = st.number_input("Stake:", value=10.0)
 
     if st.button("Calcular Risco de Ruína"):
         ruin_risk = calculate_ruin_risk(drawdown, win_rate / 100, bet_size)
